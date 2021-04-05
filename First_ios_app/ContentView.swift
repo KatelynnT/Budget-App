a91b4d034f7d5324cb96d48c9f510b5501471946
Katelynn Thorn <49296698+KatelynnT@users.noreply.github.com>
2021-04-05 16:02:02 -0700

    Initial Commit

---
+++ First_ios_app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First_ios_app
+//
+//  Created by Katelynn Thorn on 4/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First_ios_app/First_ios_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_ios_appApp.swift
+//  First_ios_app
+//
+//  Created by Katelynn Thorn on 4/5/21.
+//
+
+import SwiftUI
+
+@main
+struct First_ios_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
